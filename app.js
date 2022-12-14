17191287ea54cd61da5e6ee28bb30fc8681040ba
Kingsly Mbaaka <kingmbamu@yahoo.co.uk>
2022-12-14 06:05:16 +0100

    Disabled buttons when result reaches 5

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const selectionButtons = document.querySelectorAll("[data-selection]");
+const options = document.querySelector("[data-options");
+const selectionButtons = document.querySelectorAll("[data-option]");
+// const btn = document.getElementById("btn");
+// const btn = document.getElementsByClassName("btn");
+const btn = document.getElementsByTagName("button");
 const finalColumn = document.querySelector("[data-final-column");
 const playerScoreParagraph = document.querySelector("[data-player-score]");
 const computerScoreParagraph = document.querySelector("[data-computer-score]");
+
+let PLAYER_SCORE = 0;
+let COMPUTER_SCORE = 0;
 
 const SELECTIONS = [
   {
@@ -23,10 +30,20 @@ const SELECTIONS = [
 
 selectionButtons.forEach((selectionButton) => {
   selectionButton.addEventListener("click", (e) => {
-    const selectionName = selectionButton.dataset.selection;
+    const selectionName = selectionButton.dataset.option;
     const selection = SELECTIONS.find((selection) => selection.name === selectionName);
 
-    makeSelection(selection);
+      if (PLAYER_SCORE < 5 && COMPUTER_SCORE < 5) {
+        playRound(selection)
+      } else {
+        const div = document.createElement("div");
+        div.classList.add("result-heading");
+        div.innerText = (PLAYER_SCORE === 5) ? "Player won! Reload to restart." : "Computer won! Reload to restart."
+        options.after(div);
+        for (let i in btn) {
+          btn[i].disabled = true;
+        }
+      }
   });
 });
 
@@ -43,25 +60,27 @@ function isWinner(selection, opponentSelection) {
   return selection.beats === opponentSelection.name
 };
 
-function makeSelection(selection) {
+function playRound(selection) {
   const computerSelection = randomSelection();
   const yourWinner = isWinner(selection, computerSelection);
   const computerWinner = isWinner(computerSelection, selection)
 
   // We pass the coputer selection first because we are going to be inserting each
   // new selection at the top, immediately under the You .. Computer text
-  addSelectionResult(computerSelection, computerWinner)
-  addSelectionResult(selection, yourWinner)
+  addResult(computerSelection, computerWinner)
+  addResult(selection, yourWinner)
 
-  if (yourWinner) {
-    incrementScore(playerScoreParagraph);
+    if (yourWinner) {
+      incrementScore(playerScoreParagraph);
+      PLAYER_SCORE++
+    }
+    if (computerWinner ) {
+      incrementScore(computerScoreParagraph);
+      COMPUTER_SCORE++
+    }
   }
-  if (computerWinner) {
-    incrementScore(computerScoreParagraph);
-  }
-}
 
-function addSelectionResult(selection, winner) {
+function addResult(selection, winner) {
   const div = document.createElement("div");
   div.innerText = selection.emoji;
   div.classList.add("result-selection");
